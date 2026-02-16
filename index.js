158ba15ea33c13a483fd11f4c6ad55199386a8f2
LAKEEL\tominaga-a <tominaga-a@lakeel.com>
2026-02-16 15:26:37 +0900

    ルーティングの実装

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
 
+const routers = require("./routes");
+app.use(routers);
+
+
 app.listen(3000,() => {
   console.log("Listening on locahost port 3000");
 });
